bf8370be9c0f95a4b84c964d1bcf7689c6780bdf
Maxim Stogniy <i.dual.ce@gmail.com>
2020-05-09 00:50:15 +1200

    Skills component complete in VueJS

--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -1,1 +1,68 @@
-console.log('this is skills module');
+import Vue from "vue";
+
+const skill = {
+  template: "#skill",
+  props: {
+    name: String,
+    percent: Number
+  },
+
+  mounted() {
+    this.coloringCircle();
+  },
+
+  methods: {
+    coloringCircle() {
+      const circle = this.$refs["skill-percents"];
+      const dashArray = parseInt(
+        getComputedStyle(circle).getPropertyValue("stroke-dasharray")
+      );
+      const percent = (dashArray / 100) * (100 - this.percent);
+
+      circle.style.strokeDashoffset = percent;
+    }
+  }
+};
+
+const skillsRow = {
+  template: "#skills-row",
+  components: {
+    skill
+  },
+  props: {
+    skills: Object
+  }
+};
+
+new Vue({
+  el: "#skills-component",
+  template: "#skills-list",
+  components: {
+    skillsRow
+  },
+  
+  data() {
+    return {
+      skills: [
+        {
+          "title": "Frontend",
+          "skillGroup": {
+            "HTML5": 30,
+            "CSS3": 50,
+            "JavaScript": 25,
+            "VueJs": 30
+          }
+        },
+        {
+          "title": "Workflow",
+          "skillGroup": {
+            "GIT": 30,
+            "Terminal": 60,
+            "Gulp": 30,
+            "Webpack": 75
+          }
+        }
+      ]
+    }
+  }
+})
