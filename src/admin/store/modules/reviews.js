7b0e818270f61b9985371fc55aae29910155edb6
Maxim Stogniy <i.dual.ce@gmail.com>
2020-05-21 20:48:09 +1200

    Add files

---
+++ src/admin/store/modules/reviews.js
@@ -0,0 +1,18 @@
+import axios from '../../customAxios'
+
+export default {
+  namespaced: true,
+  
+  state: {
+    user: {}
+  },
+
+  getters: {
+  },
+
+  mutations: {
+  },
+
+  actions: {
+  }
+}
